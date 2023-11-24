b0363f3266b5e82bc5153240243217afd62fc233
김민우 <kmw@naunbiz.co.kr>
2023-11-24 17:53:37 +0900

    children 변수를 활용한 depth 처리

--- src/pages/Tags.js
+++ src/pages/Tags.js
@@ -27,18 +27,52 @@ export function Tags() {
   );
 }
 
+function InputGroup({children}) {
+  console.log(children);
+  return (
+    <form name={children.name}>
+      {children}
+    </form>
+  );
+}
+ 
+function Text(props) {
+  const type = props.numeric ? "number" : "text";
+  return (
+    <div className="input-area">
+      <label>{props.label}: <input type={type} placeholder={props.placeholder} disabled={props.disabled} maxLength={props.maxLength}></input></label>
+    </div>
+  );
+}
+
 export function InputTags() {
 
   return (
     <div className="container">
       <div>
-        <input type="text"></input>
+        <InputGroup name="skytrax">
+          <Text label="이름" disabled numeric/>
+          <Text label="코드" placeholder="react" numeric/>
+          <Text label="식별번호" placeholder="react" maxLength="4"/>
+        </InputGroup>
+        <div className="input-area">
+          <ul>
+            <li><label><input type="checkbox"></input>[필수] 이용약관 1</label></li>
+            <li><label><input type="checkbox"></input>[필수] 이용약관 2</label></li>
+            <li><label><input type="checkbox"></input>[필수] 개인 신용정보 제공 정보 동의서</label></li>
+            <li><label><input type="checkbox"></input>[선택] 마케팅 전용 개인 신용정보 제공 정보 동의서</label></li>
+          </ul>
+        </div>
+        <div className="input-area">
+          <ul>
+            <li><label><input type="radio" name="rad1"></input>남성</label></li>
+            <li><label><input type="radio" name="rad1"></input>여성</label></li>
+            <li><label><input type="radio" name="rad1"></input>기타</label></li>
+          </ul>
+        </div>
       </div>
-      <div>
+      <div className="input-area">
         <input type="file"></input>
-      </div>
-      <div>
-        <input type="multipart"></input>
       </div>
       <div>
         <a href="/">This is A Tag</a>
